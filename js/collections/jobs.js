a44c05f310f22725efb6628afb52ead90b873680
Emile Ré <nemile.re@gmail.com>
2014-08-25 01:03:20 +0200

    master lint

--- js/collections/jobs.js
+++ js/collections/jobs.js
@@ -1,15 +1,15 @@
 define([
-	'models/job',
-	'backbone.localStorage'
+    'models/job',
+    'backbone.localStorage'
 ], function (JobModel) {
 
-	var JobCollection = Backbone.Collection.extend({
-		localStorage: new Backbone.LocalStorage('collections/jobs'),
+    var JobCollection = Backbone.Collection.extend({
+        localStorage: new Backbone.LocalStorage('collections/jobs'),
 
-		model: JobModel
-	});
+        model: JobModel
+    });
 
-	// We need only one collection for the session,
-	// so we return a collection instance and not the collection model
-	return new JobCollection();
+    // We need only one collection for the session,
+    // so we return a collection instance and not the collection model
+    return new JobCollection();
 });
